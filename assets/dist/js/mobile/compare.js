10a33bc6a6a6ef3d3d66f0a5ce0c461679932254
Viktoriia <toryvna1993@gmail.com>
2019-08-06 15:03:08 +0300

    adding compare mobile

--- assets/dist/js/mobile/compare.js
+++ assets/dist/js/mobile/compare.js
@@ -10,4 +10,30 @@ $(document).ready(function(){
         CheckWindow();
     });
     //end switch url
+
+    //fixed elements
+    //fixed elements
+    var topIndex = $('.for-fixed').offset().top + $('.for-fixed').height();
+    var sum = 0;
+    $( ".for-fixed .cell" ).each(function( index ) {
+        sum+= $(this).width();
+    });
+
+    $('.table .top-heading').css('width', sum + 40);
+    $('.table .heading').css('width', sum + 40);
+
+
+    
+    $(document).scroll(function() {
+        var scroll = $(this).scrollTop();
+        if (scroll > topIndex) {
+            $('.for-fixed').addClass('fixed').css('width', sum + 75);
+        } else {
+            $('.for-fixed').removeClass('fixed');
+        }
+    });
+
+    $( ".table" ).scroll(function() {
+        $('.for-fixed.fixed').css('left', -($('.table').scrollLeft()));
+    });
 });
